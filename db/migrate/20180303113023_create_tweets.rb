f560dd8a1881973c7610932307460cd3ae01c7c3
tadahaya <cnb.jpn@gmail.com>
2018-05-30 20:35:25 +0900

    fix migrate

--- db/migrate/20180303113023_create_tweets.rb
+++ db/migrate/20180303113023_create_tweets.rb
@@ -1,10 +1,9 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-    	t.string	:id
-    	t.text	:text
-    	t.text	:image
-	    t.timestamps
+      t.text    :text
+      t.text    :image
+      t.timestamps
     end
   end
 end
